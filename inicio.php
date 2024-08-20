a0bcf5dff78227bbe5ff33d9046793d9f403a847
WhereIsJosh <77214335+WhereIsJoshhh@users.noreply.github.com>
2024-08-20 15:42:57 -0600

    Add files via upload

---
+++ inicio.php
@@ -0,0 +1,39 @@
+<?php include("plantillas/cabecera.php"); ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Carrera de Informática</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <header>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </nav>
+  </header>
+  <main>
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          <h1 class="display-4">Bienvenidos a la carrera de Informática</h1>
+          <p>En el coraz&oacute;n de la era digital, la inform&aacute;tica se erige como la fuerza impulsora que transforma nuestra manera de vivir, trabajar y conectarnos con el mundo. Desde el desarrollo de innovadoras aplicaciones m&oacute;viles hasta la creaci&oacute;n de sistemas inteligentes que desaf&iacute;an los l&iacute;mites de la tecnolog&iacute;a, la inform&aacute;tica es mucho m&aacute;s que una disciplina; es el futuro.</p>
+          <p>Nuestra misi&oacute;n es ofrecerte un espacio donde la tecnolog&iacute;a y el conocimiento convergen para inspirar a la pr&oacute;xima generaci&oacute;n de innovadores, solucionadores de problemas y l&iacute;deres en el mundo digital. Aqu&iacute;, descubrir&aacute;s c&oacute;mo la inform&aacute;tica est&aacute; presente en todos los aspectos de nuestra vida cotidiana y c&oacute;mo puedes formar parte de este emocionante viaje hacia la transformaci&oacute;n digital.</p>
+          <p>Explora los diferentes campos de la inform&aacute;tica, desde el desarrollo de software hasta la inteligencia artificial y la ciberseguridad. Conoce las &uacute;ltimas tendencias, accede a recursos educativos, y encuentra inspiraci&oacute;n para dar el siguiente paso en tu carrera o proyecto.</p>
+        </div>
+      </div>
+    </div>
+  </main>
+  <footer>
+    <div class="container">
+      <p>&copy; 2024 Mr.Doomer</p>
+    </div>
+  </footer>
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
+<?php include("plantillas/pie.php"); ?>
